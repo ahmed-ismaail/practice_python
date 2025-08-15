7fec2ea29bfa63763e342d0c0677fbf45108db26
ahmed-ismaail <ahmedmohamed.ismaail@gmail.com>
2025-08-15 14:49:23 +0300

    Operator behavior with different data types

--- practice_python.py
+++ practice_python.py
@@ -1,6 +1,7 @@
 
 print("hello world") 
 
+#variables
 age = 27
 print("I am", age, "years old")
 
@@ -9,3 +10,27 @@ print("My name is", name)
 
 is_student = False
 print("Am I a student?", is_student)
+
+
+# Arithmetic Operators
+a = 10
+b = 3
+print("Addition:", a + b)
+print("Division:", a / b)
+print("Modulus:", a % b)
+print("Exponent:", a ** b)#power
+
+# Operator behavior with different data types
+
+# Addition with numbers
+num1 = 10
+num2 = 20
+print(num1 + num2)
+
+# Concatenation with strings
+str1 = "Hello"
+str2 = "World"
+print(str1 + " " + str2)
+
+# Repetition with strings
+print(str1 * 3)
